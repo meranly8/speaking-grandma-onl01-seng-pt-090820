a6f298073c85a3814924676a051d725e938ff225
Michelle Ranly <meranly8@gmail.com>
2020-09-11 00:04:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,12 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+  if !(phrase == phrase.uppercase)
+    puts "HUH?! SPEAK UP, SONNY"
+  elseif phrase == phrase.uppercase
+    puts "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
