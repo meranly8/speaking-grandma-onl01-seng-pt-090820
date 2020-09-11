83ba087fe567238e56ab61ff272282bc0298218e
Michelle Ranly <meranly8@gmail.com>
2020-09-11 00:18:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
   elseif phrase == phrase.uppercase
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
