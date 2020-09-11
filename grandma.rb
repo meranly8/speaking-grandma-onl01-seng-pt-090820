e4d27d77f534d51df9aa6237876d207362e3c9f5
Michelle Ranly <meranly8@gmail.com>
2020-09-11 00:14:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,15 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elseif 
+  
   if !(phrase == phrase.uppercase)
     puts "HUH?! SPEAK UP, SONNY"
   elseif phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+  
   end
 end
 
